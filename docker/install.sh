cbd47e88fda27b3a7c292c5888fb0bfdc2e40d57
Dup4 <lyuzhi.pan@gmail.com>
2023-12-07 09:38:14 +0800

    style: format
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- docker/install.sh
+++ docker/install.sh
@@ -3,7 +3,7 @@
 apt update -y
 
 apt install -y ca-certificates curl gnupg \
-    htop vim git  wget openjdk-8-jdk-headless maven
+    htop vim git wget openjdk-8-jdk-headless maven
 
 cd /live/icpc-live-v2 || exit 1
 
